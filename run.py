d75fb4242b87b3af117dab929781a113efcf808f
Waris Radji <wradji64@gmail.com>
2024-01-29 01:22:53 +0100

    One map

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-import functools
 import multiprocessing
 import os
 import subprocess
 import sys
 
 
-def run(seed, skeleton: bool = False):
-    # Replace 'your_program.py' with the name of the Python program you want to run
+def run(args):
+    seed, skeleton = args
+
     program_command = [
         'python',
         'cleanrl/dqn.py' if not skeleton else 'cleanrl/skeleton_dqn.py',
@@ -19,18 +19,17 @@ def run(seed, skeleton: bool = False):
     my_env = os.environ.copy()
     my_env["CUDA_VISIBLE_DEVICES"] = "0" if not skeleton else "1"
 
-
-    # Run the program using subprocess
     subprocess.run(program_command, env=my_env)
 
 
 if __name__ == "__main__":
     # Define the range of seeds
     seeds = range(8)
+    skeletons = [False, True]
+    arg_combinations = [(seed, skeleton) for seed in seeds for skeleton in skeletons]
 
     # Create a multiprocessing pool with the number of desired processes
     # You can adjust the number of processes based on your system's capabilities
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
         # Use the pool.map function to distribute the seeds among the processes
-        pool.map_async(functools.partial(run, skeleton=False), seeds)
-        pool.map_async(functools.partial(run, skeleton=True), seeds)
+        pool.map(run, arg_combinations)
